39625e88e9ebc4bfa4901dca55818f6e360cbb32
Ian Bennett <subtropicalhorseback@gmail.com>
2024-02-05 12:00:03 -0600

    intro written for attack.py

---
+++ attack.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# This script exists to document two functions we've already written in class- the network enumeration and port scanner, and the brute force attempter;
+# it also serves to house two new options, a simulated exfiltration and an anomaly based code execution
+
+# members of the team include Ian Bennett, Cody Juhl, Kevin Hoang, Brittany Powell, Juan Maldonado, and Marcus Nogueira. 
+# primary author of this script is Ian Bennett
+
+####################### 
+##### IMPORTS #########
+#######################
+
